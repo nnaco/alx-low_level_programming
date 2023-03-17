3bb84f77a8f74297490747b5f8309a60a7e3d732
nnaco <nnaco24@gmail.com>
2023-03-16 19:13:39 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * description; main prints out a string to stdout.
+ * return: 0 if success.
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
