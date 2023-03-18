d9c748a29eeed8fdbdab476c746589e9356b681a
nnaco <nnaco24@gmail.com>
2023-03-18 08:14:13 -0700

    print alphabets except q and e

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -4,7 +4,7 @@
  * main - Entry point
  *
  * Return: Always 0 (success)
- */
+ * */
 int main(void)
 {
 int lowercase = 'a';
@@ -14,7 +14,7 @@ if (lowercase == 'e' || lowercase == 'q')
 {
 lowercase += 1;
 }
-else 
+else
 {
 putchar(lowercase);
 lowercase += 1;
