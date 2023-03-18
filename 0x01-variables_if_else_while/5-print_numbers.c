a0b25b73f4842b62628326873eb9bc1b95494b36
nnaco <nnaco24@gmail.com>
2023-03-18 08:47:32 -0700

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+int a = 0;
+while (a<10)
+{
+printf("a is: %d\n", a);
+a =a+1;
+}
+return (0);
+}
