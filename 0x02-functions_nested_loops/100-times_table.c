a83b392e94d9ff913afdc375f9d00730aab391f4
nnaco <nnaco24@gmail.com>
2023-03-23 10:49:46 +0000

    times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,10 +6,10 @@
  */
 void print_times_table(int n)
 {
-int num,mult, prod;
+int num, mult, prod;
 if (n >= 0 && n <= 15)
 {
-for (num = 0; num <= n; num ++)
+for (num = 0; num <= n; num++)
 {
 _putchar('0');
 for (mult = 1; mult <= n; mult++)
@@ -24,7 +24,7 @@ _putchar(' ');
 if (prod >= 100)
 {
 _putchar((prod / 100) + '0');
-_putchar(((prod/ 10)) % 10 + '0');
+_putchar(((prod / 10)) % 10 + '0');
 }
 else if (prod <= 99 && prod >= 10)
 {
@@ -35,5 +35,4 @@ _putchar((prod % 10) + '0');
 _putchar('\n');
 }
 }
-}
-					
+}			
