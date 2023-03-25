237a351cdba7f2e9509ffb33d807252b8295720d
nnaco <nnaco24@gmail.com>
2023-03-25 08:04:43 +0000

    diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,22 +7,24 @@
 
 void print_diagonal(int n)
 {
-if (n <= 0)
-{
-_putchar('\n');
-}else
-{
-int i, j;
-for (i = 0; i < n; i++)
-{
-for (j = 0; j < n; j++)
-{
-if (j == i)
-_putchar('\\');
-else if (j < i)
-_putchar(' ');
+	if (n <= 0)
+	{
+		_putchar('\n');
+	} else
+	{
+		int i, j;
+
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < n; j++)
+			{
+				if (j == i)
+					_putchar('\\');
+				else if (j < i)
+					_putchar(' ');
+			}
+			_putchar('\n');
+		}
+	}
 }
-_putchar('\n');
-}
-}
-}
+
