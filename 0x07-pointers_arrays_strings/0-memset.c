b6e2a3dd4b9fb64c7ad08ad01f1a05c7213a53f7
nnaco <nnaco24@gmail.com>
2023-04-03 23:38:28 +0000

    memorys

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -3,16 +3,17 @@
 /**
  * Entry - fill a block of memory with a specific value
  *
+ *
  * Return: changed array with new value for n bytes
  */
 
 char *_memset(char *s, char b, unsigned int n)
 {
-int a = 0;
-for(; n > 0; a++)
-{
-s[a] = b;
-n--;
-}
+	int a = 0;
+	for(; n > 0; a++)
+	{
+		s[a] = b;
+		n--;
+	}
 return(s);
 }
