3dd750163b5fc0119fdfac3a40ba8e7fd4ff9f9d
nnaco <nnaco24@gmail.com>
2023-04-04 00:55:53 +0000

    string searcher

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strpbrk - Locates the first occurence in the string s
+ * @s - input
+ * @accept - input
+ *
+ * Return: Always 0 (Success)
+ */
+char *_strpbrk(char *s, char *accept)
+{
+int i;
+while (*s)
+{
+for (i = 0; accept[i]; i++)
+{if (*s == accept[i])
+return (s);
+}
+s++;
+}
+return ('\0');
+}
