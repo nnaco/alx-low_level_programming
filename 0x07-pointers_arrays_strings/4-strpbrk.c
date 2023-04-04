62a801d9524e3a27421a898fcbc9aabf4c850978
nnaco <nnaco24@gmail.com>
2023-04-04 01:00:40 +0000

    string locator

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -6,13 +6,15 @@
  *
  * Return: Always 0 (Success)
  */
+
 char *_strpbrk(char *s, char *accept)
 {
 int i;
 while (*s)
 {
 for (i = 0; accept[i]; i++)
-{if (*s == accept[i])
+{
+if (*s == accept[i])
 return (s);
 }
 s++;
