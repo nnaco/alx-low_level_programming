a26ccf3d065a7fb7fc82a0742621c408b960da33
nnaco <nnaco24@gmail.com>
2023-04-06 13:28:34 +0000

    recussions

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,17 +1,17 @@
 #include "main.h"
 /**
  * _puts_recursion - Prints a string.
- * @s: input
+ * @*s: input
  *
  * Return: Always 0 (success)
  */
 void _puts_recursion(char *s)
 {
-	if (*s)
-	{
-		_puts_recursion(s + 1);
-		_putchar(*s);
-	}
-	else
-		_putchar('\n');
+if (*s == '\0')
+{
+         _putchar('\n');
+         return;
+}
+_putchar(*s);
+_puts_recursion(s + 1);
 }
--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,18 +1,14 @@
 #include "main.h"
 /**
- * _strlen_recursion - Returns the length of a string.
- * @s: The string to be measured.
+ * _print_rev_recursion - prints a reverse string
+ * @s: the value of the string
  *
- * Return: The length of the string.
  */
-int _strlen_recursion(char *s)
+void _print_rev_recursion(char *s)
 {
-int longit = 0;
-
-if (*s)
-{
-longit++;
-longit += _strlen_recursion(s + 1);
-}
-return (longit);
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
 }
