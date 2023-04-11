dc4fcf922944b597c569ab61cc7147acd2bec451
nnaco <nnaco24@gmail.com>
2023-04-11 03:14:46 +0000

    static libraries

--- 0x09-static_libraries/4-isalpha.c
+++ 0x09-static_libraries/4-isalpha.c
@@ -1,22 +1,11 @@
 #include "main.h"
 
 /**
- * main - check the code.
- *
- * Return: Always 0.
+ * _isalpha - checks for alphabetic character
+ * @c: the character to be checked
+ * Return: 1 if c is a letter, 0 otherwise
  */
-int main(void)
+int _isalpha(int c)
 {
-    int d;
-
-d = _isalpha('H');
-_putchar(d + '0');
-d = _isalpha('o');
-_putchar(d + '0');
-d = _isalpha(108);
-_putchar(d + '0');
-d = _isalpha(';');
-_putchar(d + '0');
-_putchar('\n');
-return (0);
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
 }
