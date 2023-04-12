434dd70562de89987555afa0f7f47a9c4eed1fea
nnaco <nnaco24@gmail.com>
2023-04-12 00:39:42 +0000

    memory allocation

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * free_grid - frees 2d array
+ * @grid: 2d grid
+ * @height: height dimension of grid
+ * 
+ * Return: nothing
+ */
+void free_grid(int **grid, int height)
+{
+int fg;
+for (fg = 0; fg < height; fg++)
+{
+free(grid[fg]);
+}
+free(grid);
+}
