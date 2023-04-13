fa6b744aa70b5ace1c1fa2824d23616d13c1df46
nnaco <nnaco24@gmail.com>
2023-04-13 01:38:11 +0000

    all arguments

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -8,12 +8,13 @@
   *
   *Return: Always 0 (success)
   */
+
 int main(int argc, char *argv[])
 {
-	int a;
-	for (a = 0; a < argc; a++)
-	{
-		printf("%s\n", argv[a]);
-	}
-	return (0);
+int a;
+for (a = 0; a < argc; a++)
+{
+printf("%s\n", argv[a]);
+}
+return (0);
 }
