1ba8ca505dd7dd25afe35fc6781bb73691fee107
nnaco <nnaco24@gmail.com>
2023-04-20 05:34:43 +0000

    function pointers

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,14 +3,14 @@
 
 /**
   * print_name - print name using pointer to function
-  * @name: string 
+  * @name: string
   * @f: pointer
   * Return: nothing
   **/
 
 void print_name(char *name, void (*f)(char *))
 {
-	if(name == NULL || f == NULL)
+	if (name == NULL || f == NULL)
 		return;
 	f(name);
 }
